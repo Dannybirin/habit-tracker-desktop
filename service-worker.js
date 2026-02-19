88f059394cc569ee0a7fcdecce5add4eb83a2ff8
Daniel Birin <danielbirin7@gmail.com>
2026-02-19 21:15:22 +0000

    Initial commit: Habit Tracker Desktop

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "habit-tracker-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js");
+}
+
